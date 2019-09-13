66ba226ea20ead5eb01feaaa2f10be3d6819c0c2
Guy <ghaisanlatheef98@gmail.com>
2019-09-13 10:42:25 +0500

    supports more symbols with raw strings

--- lib/base_converter.dart
+++ lib/base_converter.dart
@@ -42,7 +42,8 @@ class BaseConverter {
     return power;
   }
 
-  /// 0-9 [0-9], 10-35 [A-Z], 36-61 [a-z] , 62-93 symbols
+  /// 0-9 [0-9], 10-35 [A-Z], 36-61 [a-z] , 62-92 symbols
+  /// Thus, base is (last desired symbol index + 1). Example, for numerics, as 9 is the last digit, add 1 to get base10. Numerics + capitals = base36. Alphanumeric = base62. All_Printable_Chars = base93.
   static const List<String> refTable = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
     'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
     'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
--- test/base_converter_test.dart
+++ test/base_converter_test.dart
@@ -2,8 +2,8 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:base_converter/base_converter.dart';
 
 void main() {
-  test('adds one to input values', () {
-    print(BaseConverter.itoa(180, 93));
-    print(BaseConverter.atoi("UQpwIuh", 61));
+  test('Converts an int to string of given base and vice versa', () {
+    print(BaseConverter.itoa(1568290107882, 62));
+    print(BaseConverter.atoi("RbrFT22", 62));
   });
 }
